c910af45dd310cc55a19841c5dce146d1347f015
Alexis <35.alexis.pele@gmail.com>
2025-04-11 21:38:21 +0200

    Added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    server: { port: 1312 }
+    server: { port: 1312 },
+    vite: {
+        plugins: [
+            tailwindcss()
+        ]
+    }
 });
