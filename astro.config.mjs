83f01892bea89e534c250b62333fdb8e3f0ddd62
Alexis <35.alexis.pele@gmail.com>
2025-04-11 21:40:27 +0200

    Added sitemap and compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from '@astrojs/sitemap';
+import compressor from 'astro-compressor';
+
 // https://astro.build/config
 export default defineConfig({
     server: { port: 1312 },
     vite: {
         plugins: [
+            sitemap(),
+            compressor(),
             tailwindcss()
         ]
     }
